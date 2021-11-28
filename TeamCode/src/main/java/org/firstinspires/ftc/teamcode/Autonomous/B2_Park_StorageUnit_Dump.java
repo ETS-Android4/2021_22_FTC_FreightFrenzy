cb24a5033cc4b65095f7eeb6d49be5d8e45fa4f9
Hannah Zullow <kkz4@verizon.net>
2021-11-28 12:41:18 -0500

    Added to B2 to deliver freight into the alliance storage unit
    0. Initialize robot (insert freight)
    1. Move forward a couple inches (6.5 inches)
    2. Turn right 90 degrees
    3. Move forward to the wall of the storage unit (5 inches)
    4. Stop all holonomic drive motors
    5. Deposit freight in storage unit
    6. Stop all intake motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/B2_Park_StorageUnit_Dump.java
@@ -0,0 +1,240 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.HardwareMap.HardwareMap_Holonomic;
+import org.firstinspires.ftc.teamcode.HardwareMap.HardwareMap_RackAndPinion;
+
+
+/**
+ * This file illustrates the concept of driving a path based on encoder counts.
+ * It uses the common Pushbot hardware class to define the drive on the robot.
+ * The code is structured as a LinearOpMode
+ *
+ * The code REQUIRES that you DO have encoders on the wheels,
+ *   otherwise you would use: PushbotAutoDriveByTime;
+ *
+ *  This code ALSO requires that the drive Motors have been configured such that a positive
+ *  power command moves them forwards, and causes the encoders to count UP.
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 48 inches
+ *   - Spin right for 12 Inches
+ *   - Drive Backwards for 24 inches
+ *   - Stop and close the claw.
+ *
+ *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
+ *  that performs the actual movement.
+ *  This methods assumes that each movement is relative to the last stopping place.
+ *  There are other ways to perform encoder based moves, but this method is probably the simplest.
+ *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="B2 Park StorageUnit Dump", group="Blue")
+//@Disabled
+public class B2_Park_StorageUnit_Dump extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    HardwareMap_RackAndPinion robot   = new HardwareMap_RackAndPinion();   // Use a Pushbot's hardware
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+    @Override
+    public void runOpMode() {
+
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        int state = 0;
+        if (state == 0){
+            //init robot
+            robot.init(hardwareMap);
+
+
+            // Send telemetry message to signify robot waiting;
+            telemetry.addData("Status", "Resetting Encoders");    //
+            telemetry.update();
+
+            robot.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            robot.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            // Send telemetry message to indicate successful Encoder reset
+            telemetry.addData("Path0",  "Starting at %7d :%7d",
+                    robot.leftFront.getCurrentPosition(),
+                    robot.rightFront.getCurrentPosition());
+            telemetry.update();
+
+
+            waitForStart();
+            state = 1;
+        }
+
+        //moving forward several inches
+        if (state == 1){
+            telemetry.addData("State","1");
+            telemetry.update();
+            encoderDrive(DRIVE_SPEED, 6.5, 6.5,6.5, 6.5, 4.0);
+            //facing Forward strafe left one foot.
+            state = 2;
+        }
+        //turning right
+        if (state == 2) {
+            telemetry.addData("State","2");
+            telemetry.update();
+            encoderDrive(DRIVE_SPEED, 5, -5, 5, -5, 4.0);
+            state = 3;
+        }
+        //moving forward several inches
+        if (state == 3) {
+            telemetry.addData("State", "3");
+            telemetry.update();
+            encoderDrive(DRIVE_SPEED, 5, 5, 5, 5, 4.0);
+            //Move forward six feet.
+            state = 4;
+        }
+        //stopping robot
+        if(state == 4){
+            telemetry.addData("State", "4");
+            telemetry.update();
+            robot.leftFront.setPower(0);
+            robot.rightFront.setPower(0);
+            robot.leftBack.setPower(0);
+            robot.rightBack.setPower(0);
+
+            //Move forward six feet.
+            state = 5;
+        }
+        if(state == 5){
+            telemetry.addData("State", "4");
+            telemetry.update();
+            retractFreight(5, 1);
+            //Move forward six feet.
+            state = 6;
+        }
+        if(state == 6){
+        telemetry.addData("State", "4");
+        telemetry.update();
+        robot.leftIntake.setPower(0);
+        robot.rightIntake.setPower(0);
+        //Move forward six feet.
+        state = 7;
+        }
+        //stop all motion
+        // encoderDrive(  // S3: Reverse 24 Inches with 4 Sec timeout
+
+        // robot.leftClaw.setPosition(1.0);            // S4: Stop and close the claw.
+        // robot.rightClaw.setPosition(0.0);
+        sleep(1000);     // pause for servos to move
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+
+
+
+
+    }
+
+    /*
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches, double leftBackInches, double rightBackInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+        int newLeftBackTarget;
+        int newRightBackTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = robot.leftFront.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = robot.rightFront.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newLeftBackTarget = robot.leftBack.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightBackTarget = robot.rightBack.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            robot.leftFront.setTargetPosition(newLeftTarget);
+            robot.rightFront.setTargetPosition(newRightTarget);
+            robot.leftBack.setTargetPosition(newLeftBackTarget);
+            robot.rightBack.setTargetPosition(newRightBackTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftFront.setPower(Math.abs(speed));
+            robot.rightFront.setPower(Math.abs(speed));
+            robot.leftBack.setPower(Math.abs(speed));
+            robot.rightBack.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.leftFront.isBusy() && robot.rightFront.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        robot.leftFront.getCurrentPosition(),
+                        robot.rightFront.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            robot.leftFront.setPower(0);
+            robot.rightFront.setPower(0);
+            robot.leftBack.setPower(0);
+            robot.rightBack.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            robot.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
+    }
+    public void retractFreight(double freightTime, double freightSpeed) {
+        while (opModeIsActive() &&
+                (runtime.seconds() < freightTime)){
+            robot.leftIntake.setPower(freightSpeed);
+            robot.rightIntake.setPower(freightSpeed);
+        }
+    }
+}
